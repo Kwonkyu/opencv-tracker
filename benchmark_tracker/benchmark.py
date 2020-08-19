0578e91b539951def79beeb494a586e02790743c
Kwonkyu <park2348190@naver.com>
2020-08-19 09:57:58 +0900

    modified code and implemented feature
    # benchmark.py
    - removed redundant variable
    - optimized codes
    - implemented feature to write out tracker's tracking result(benchmark) as text file(bounding box's xywh with commas).
    
    Signed-off-by: Kwonkyu <park2348190@naver.com>

--- benchmark_tracker/benchmark.py
+++ benchmark_tracker/benchmark.py
@@ -19,21 +19,20 @@ argparser.add_argument("-v", "--video", type=str, required=True, help="benchmark
 argparser.add_argument("-g", "--ground-truth", type=str, required=True, help="ground truth file")
 argparser.add_argument("-t", "--tracker", type=str, required=True,  # nargs='*',
                        help="benchmark tracker type input(BOOSTING CSRT GOTURN KCF MEDIANFLOW MOSSE TLD).")
-argparser.add_argument("-n", "--no-status", action='store_true', help="option to hide tracking status.")
-argparser.add_argument("-s", "--split", action='store_true', help="option to split ground truth and tracking output.")
-argparser.add_argument("-c", "--capture", action="store_true", help="option to export result to video(avi).")
-argparser.add_argument("-b", "--verbose", action="store_true", help="option to show benchmark result in window.")
+argparser.add_argument("--no-status", action='store_true', help="option to hide tracking status.")
+argparser.add_argument("--split", action='store_true', help="option to split ground truth and tracking output.")
+argparser.add_argument("--output", action="store_true", help="option to export result to video(avi).")
+argparser.add_argument("--benchmark-output", action="store_true", help="option to export benchmark result in txt.")
 args = vars(argparser.parse_args())
 
 # Video input, output related variables.
-is_video_write = args['capture']
 video_input = cv2.VideoCapture(args['video'])
 video_width = int(video_input.get(cv2.CAP_PROP_FRAME_WIDTH))
 video_height = int(video_input.get(cv2.CAP_PROP_FRAME_HEIGHT))
 video_name = "benchmark-{}-{}-{}.avi".format(args['tracker'], os.path.basename(str(args['video'])),
                                              datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
 video_writer = cv2.VideoWriter(video_name, cv2.CAP_FFMPEG, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
-                               25, (video_width, video_height)) if is_video_write else None
+                               25, (video_width, video_height)) if args['output'] else None
 
 # Ground-truth related variables.
 ground_truth_file = open(args['ground_truth'], 'r')
@@ -44,7 +43,9 @@ ground_truth_file.close()
 is_tracker_tracking = True
 is_status_hide = args['no_status']
 is_split_frame = args['split']
-is_verbose_window = args['verbose']
+benchmark_text_name = "benchmark-{}-{}-{}.txt".format(args['tracker'], os.path.basename(str(args['video'])),
+                                                      datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+benchmark_writer = open(benchmark_text_name, "w") if args['benchmark_output'] else None
 tracker = None
 
 # Start from 0# frame of video.
@@ -71,9 +72,12 @@ while video_input.isOpened():
         # If tracker is already initialized, update tracker and get result coordinates
         # to draw rectangle on tracking frame.
         is_tracker_tracking, (x, y, w, h) = tracker.update(tracking_frame)
+        (x, y, w, h) = (int(x), int(y), int(w), int(h))
         if is_tracker_tracking:
-            (x, y, w, h) = (int(x), int(y), int(w), int(h))
             cv2.rectangle(tracking_frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    if benchmark_writer is not None:
+        benchmark_writer.write("{},{},{},{}\n".format(x, y, w, h))
 
     # Draw a ground-truth rectangle based on given text file. It assume that text file has 4 numbers
     # on each line separated by comma(,). These numbers are x, y, width, height of rectangle.
@@ -98,17 +102,16 @@ while video_input.isOpened():
         cv2.putText(tracking_frame, tracking_status_string, (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255))
 
     # If verbose option was set, draw benchmark result in window.
-    if is_verbose_window:
-        cv2.imshow("benchmark", video_frame)
-        if is_split_frame:
-            cv2.imshow("tracking", tracking_frame)
+    cv2.imshow("benchmark", video_frame)
+    if is_split_frame:
+        cv2.imshow("tracking", tracking_frame)
 
     # If video output option was set, draw ground-truth rectangle to tracking frame and write to video file.
     # It's because window is may split by option and so ground-truth rectangle, tracking output rectangle are
     # drawed on separate window. x1, y1, x2, y2 variables which are coordinates of ground-truth rectangle
     # keep their values to the end of this loop. So drawing ground-truth rectangle to tracking frame
     # won't be any problem. Remember, tracking frame is video frame when there's no split option was set.
-    if is_video_write and video_writer is not None:
+    if video_writer is not None:
         cv2.rectangle(tracking_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
         video_writer.write(tracking_frame)
 
@@ -117,5 +120,6 @@ while video_input.isOpened():
     if key == ord("q"):
         break
 
+benchmark_writer.close()
 video_input.release()
 cv2.destroyAllWindows()
